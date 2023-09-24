5ca2173c94dceb77095ab0a7be0fd66a7a8a4d90
CrackTC <chenrke369@gmail.com>
2023-09-24 11:03:27 +0800

    add gstreamer

--- home/game/default.nix
+++ home/game/default.nix
@@ -2,5 +2,17 @@
   home.packages = with extraRepos; with pkgs; [
     myRepo.beatoraja
     yuzu-mainline
+    # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+    gst_all_1.gstreamer
+    # Common plugins like "filesrc" to combine within e.g. gst-launch
+    gst_all_1.gst-plugins-base
+    # Specialized plugins separated by quality
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    # Plugins to reuse ffmpeg to play almost every video format
+    gst_all_1.gst-libav
+    # Support the Video Audio (Hardware) Acceleration API
+    gst_all_1.gst-vaapi
   ];
 }
