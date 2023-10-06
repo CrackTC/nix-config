081032413a4dbedd7f68cb5d539b202925a1d646
CrackTC <chenrke369@gmail.com>
2023-10-06 20:17:42 +0800

    fish: vi mode

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -14,6 +14,30 @@
     ];
     functions = {
       fish_greeting = "";
+      fish_user_key_bindings = ''
+        fish_default_key_bindings -M insert
+        fish_vi_key_bindings --no-erase insert
+
+        bind n down-or-search
+        bind -M visual n down-or-search
+
+        bind e up-or-search
+        bind -M visual e up-or-search
+
+        bind i forward-char
+        bind -M visual i forward-char
+
+        bind -m insert u repaint-mode
+        bind -m insert U beginning-of-line repaint-mode
+
+        bind l undo
+        bind \cp redo
+
+        bind -m insert y insert-line-under repaint-mode
+        bind -m insert Y insert-line-over repaint-mode
+
+        bind ss kill-whole-line
+      '';
       enbase = "echo -n $argv | base64";
       debase = "echo -n $argv | base64 -d";
       miniconda = "conda-shell -c 'fish -C \"conda shell.fish hook $argv | source\"'";
