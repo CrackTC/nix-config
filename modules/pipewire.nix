9c706bbf1947fd8997693f50d970455a9f6a8f54
CrackTC <chenrke369@gmail.com>
2023-10-09 00:29:54 +0800

    pipewire: force 44100Hz

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -6,4 +6,14 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  environment.etc = {
+    "pipewire/pipewire.conf.d/99-pipewire.conf" = {
+      text = ''
+      context.properties = {
+        default.clock.rate = 44100;
+        default.clock.allowed-rates = [ 44100 ]
+      };
+      '';
+    };
+  };
 }
