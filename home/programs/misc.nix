7c293b516dc556c6a14714d94095f60caba8d80e
CrackTC <chenrke369@gmail.com>
2023-10-12 16:25:15 +0800

    add wechat-uos-bin

--- home/programs/misc.nix
+++ home/programs/misc.nix
@@ -25,5 +25,14 @@
     wpsoffice
 
     extraRepos.myRepo.freedownloadmanager
+
+    (extraRepos.nur.repos.xddxdd.wechat-uos-bin.overrideAttrs
+      (oldAttrs: {
+        src = pkgs.fetchurl {
+          url = "https://home-store-packages.uniontech.com/appstore/pool/appstore/c/com.tencent.weixin/com.tencent.weixin_2.1.5_amd64.deb";
+          sha256 = "sha256-dDoWP6RZAYKOUF6/BEpNBD587HCDjkiu4AhU5XCx2WE=";
+        };
+      })
+    )
   ];
 }
