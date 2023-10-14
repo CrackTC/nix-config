d15a232c94c5b5b8afd3c66ae203657f361ad226
CrackTC <chenrke369@gmail.com>
2023-10-14 11:14:34 +0800

    nvim: fix kbd

--- home/programs/nvim/config/lua/post-keybindings.lua
+++ home/programs/nvim/config/lua/post-keybindings.lua
@@ -14,3 +14,4 @@ utils.vnoremap("s", "d", { nowait = true })
 utils.nnoremap("ds", "<Plug>(coc-definition)", { silent = true })
 
 utils.nnoremap("<C-p>", "<C-r>")
+utils.nnoremap("p", "<Plug>(SubversiveSubstitute)", { silent = true });
