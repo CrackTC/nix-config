7b386aab0e69c8d1992ad6afbf8a00dd8d9208fe
CrackTC <chenrke369@gmail.com>
2023-10-14 11:14:10 +0800

    enable powermanagement

--- host/cno/default.nix
+++ host/cno/default.nix
@@ -104,6 +104,22 @@
     };
   };
 
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
+
   security.sudo.extraRules = [
     {
       users = [ "${info.username}" ];
