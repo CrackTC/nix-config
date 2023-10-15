ca834d6eb0fc0c384eef244366c1af14ac143ba4
CrackTC <chenrke369@gmail.com>
2023-10-15 12:33:57 +0800

    hyprland: monitor config

--- home/hypr/noExtMonitor.nix
+++ home/hypr/noExtMonitor.nix
@@ -1,7 +1,7 @@
 { lib, ... }: {
   wayland.windowManager.hyprland.settings.monitor = lib.mkForce [
-    "DP-1,disabled"
-    "eDP-1,2560x1440@240,0x0,1"
+    "DP-2,2560x1440@170,0x0,1"
+    "eDP-1,2560x1440@240,2560x0,1"
     "monitor=HDMI-A-1,disabled"
   ];
 }
