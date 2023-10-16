890135005026e756c461fb8ba02f360dfddee275
CrackTC <chenrke369@gmail.com>
2023-10-17 00:09:27 +0800

    hyprland: choose integrated gpu explicitly

--- home/hypr/intel.nix
+++ home/hypr/intel.nix
@@ -1,5 +1,6 @@
 {
   wayland.windowManager.hyprland.settings.env = [
     "__EGL_VENDOR_LIBRARY_FILENAMES,/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json"
+    "WLR_DRM_DEVICES,/dev/dri/card1"
   ];
 }
