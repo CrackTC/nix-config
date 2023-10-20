07023a88120b9a71cea239e8d124dfbac6788bec
CrackTC <chenrke369@gmail.com>
2023-10-20 23:32:26 +0800

    split fhs.nix & add dotnet static linking deps

---
+++ home/dev/fhs.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    (pkgs.buildFHSUserEnv {
+      name = "fhs";
+      runScript = "fish";
+      targetPkgs = pkgs: with pkgs; [
+        # for machine learning
+        cudatoolkit
+        cudaPackages.cudnn
+        openssl_1_1
+        libGL
+
+        # for dotnet static linking
+        glibc.static
+        zlib.static
+        libffi
+        libtool
+        musl
+        ghc
+        gcc
+        ocaml
+      ];
+    })
+  ];
+}
