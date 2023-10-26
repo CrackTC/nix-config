fa703601ed1ecc4bdd95acbb0e73d2ea59daf1f8
CrackTC <chenrke369@gmail.com>
2023-10-26 10:56:27 +0800

    docker ustc registry

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,4 +1,13 @@
 { info, ... }: {
   virtualisation.docker.enable = true;
   users.users.${info.username}.extraGroups = [ "docker" ];
+  environment.etc = {
+    "docker/daemon.json" = {
+      text = ''
+      {
+        "registry-mirrors": [ "https://docker.mirrors.ustc.edu.cn/" ]
+      }
+      '';
+    };
+  };
 }
