fcd3cfec5f0b94be26631c38814df8d43d4efbd4
CrackTC <chenrke369@gmail.com>
2023-11-10 11:29:53 +0800

    dotnet: multiple sdks

--- home/dev/dotnet.nix
+++ home/dev/dotnet.nix
@@ -1,6 +1,9 @@
-{pkgs, ...} : {
-    home.packages = with pkgs; [
-      dotnet-sdk_7
-      csharp-ls
-    ];
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    (with dotnetCorePackages; combinePackages [
+      sdk_7_0
+      sdk_6_0
+    ])
+    csharp-ls
+  ];
 }
