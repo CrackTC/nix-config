d33fcf3a992339666ac123ff9135617643e69b50
CrackTC <chenrke369@gmail.com>
2023-12-03 01:42:31 +0800

    firefox: increase scale

--- home/programs/firefox/settings/default.nix
+++ home/programs/firefox/settings/default.nix
@@ -6,7 +6,7 @@ profileName: {
   programs.firefox.profiles.${profileName}.settings = {
     "trailhead.firstrun.didSeeAboutWelcome" = true;
     "intl.locale.requested" = "zh-CN,en-US";
-    "layout.css.devPixelsPerPx" = 1.25;
+    "layout.css.devPixelsPerPx" = 1.5;
     "signon.rememberSignons" = false;
     "browser.tabs.tabmanager.enabled" = false;
   };
