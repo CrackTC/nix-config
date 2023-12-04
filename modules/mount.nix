0b39155271df334562873e3083d61017990e49cf
CrackTC <chenrke369@gmail.com>
2023-12-05 02:15:16 +0800

    add nfs mount

--- modules/mount.nix
+++ modules/mount.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/home/chen/nas" = {
+  fileSystems."/mnt/smb/nas" = {
     device = "//cracktc.tpddns.cn/nas";
     fsType = "cifs";
     options =
@@ -8,5 +8,9 @@
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
       [ "${automount_opts},credentials=/run/secrets/nas_credentials,iocharset=utf8,uid=1000,gid=100,sec=ntlmssp,rw,port=446" ];
+  };
+  fileSystems."/home/chen/nas" = {
+    device = "cracktc.tpddns.cn:/volume1/nas";
+    fsType = "nfs";
   };
 }
