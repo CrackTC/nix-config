f6a381435e11e9e2cdb37767640f769f520c1bff
CrackTC <chenrke369@gmail.com>
2023-12-05 02:12:48 +0800

    xdg: firefox as default browser

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -1,11 +1,20 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
+  # overwrite existing mimeapps.list
   xdg.configFile."mimeapps.list".force = true;
-  xdg.mimeApps.enable = true;
-  xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/http" = "vivaldi-stable.desktop";
-    "x-scheme-handler/https" = "vivaldi-stable.desktop";
-    "application/pdf" = "okularApplication_pdf.desktop";
-    "text/plain" = "nvim.desktop";
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications =
+      lib.genAttrs [
+        "text/html"
+        "x-scheme-handler/http"
+        "x-scheme-handler/https"
+        "x-scheme-handler/about"
+        "x-scheme-handler/unknown"
+      ]
+        (mime: "firefox.desktop")
+
+      // lib.genAttrs [ "application/pdf" ]
+        (mime: "okularApplication_pdf.desktop");
   };
 
   home.packages = with pkgs; [ xdg-utils ];
