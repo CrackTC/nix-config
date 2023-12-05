e647a9cdcc54e109017ec0880b9f9cc5798ae057
CrackTC <chenrke369@gmail.com>
2023-12-05 22:56:09 +0800

    mount: fix & clean

--- modules/mount.nix
+++ modules/mount.nix
@@ -5,12 +5,27 @@
     fsType = "cifs";
     options =
       let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        automount_opts = [
+          "x-systemd.automount"
+          "noauto"
+          "x-systemd.idle-timeout=60"
+          "x-systemd.device-timeout=5s"
+          "x-systemd.mount-timeout=5s"
+        ];
       in
-      [ "${automount_opts},credentials=/run/secrets/nas_credentials,iocharset=utf8,uid=1000,gid=100,sec=ntlmssp,rw,port=446" ];
+      automount_opts ++ [
+        "credentials=/run/secrets/nas_credentials"
+        "iocharset=utf8"
+        "uid=1000"
+        "gid=100"
+        "sec=ntlmssp"
+        "rw"
+        "port=446"
+      ];
   };
   fileSystems."/home/chen/nas" = {
     device = "cracktc.tpddns.cn:/volume1/nas";
     fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" ];
   };
 }
