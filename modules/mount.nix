2b5daf7a673546387a0c559f8c37612414d9d3c7
CrackTC <chenrke369@gmail.com>
2023-12-12 02:20:24 +0800

    add windows partition mount

--- modules/mount.nix
+++ modules/mount.nix
@@ -1,31 +1,45 @@
 { pkgs, ... }: {
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/smb/nas" = {
-    device = "//cracktc.tpddns.cn/nas";
-    fsType = "cifs";
-    options =
-      let
-        automount_opts = [
-          "x-systemd.automount"
-          "noauto"
-          "x-systemd.idle-timeout=60"
-          "x-systemd.device-timeout=5s"
-          "x-systemd.mount-timeout=5s"
+  fileSystems = {
+    "/mnt/windows" = {
+      device = "/dev/disk/by-uuid/C642192442191B2F";
+      fsType = "ntfs";
+    };
+
+    "/mnt/smb/nas" = {
+      device = "//cracktc.tpddns.cn/nas";
+      fsType = "cifs";
+      options =
+        let
+          automount_opts = [
+            "x-systemd.automount"
+            "noauto"
+            "x-systemd.idle-timeout=60"
+            "x-systemd.device-timeout=5s"
+            "x-systemd.mount-timeout=5s"
+          ];
+        in
+        automount_opts ++ [
+          "credentials=/run/secrets/nas_credentials"
+          "iocharset=utf8"
+          "uid=1000"
+          "gid=100"
+          "sec=ntlmssp"
+          "rw"
+          "port=446"
         ];
-      in
-      automount_opts ++ [
-        "credentials=/run/secrets/nas_credentials"
-        "iocharset=utf8"
-        "uid=1000"
-        "gid=100"
-        "sec=ntlmssp"
-        "rw"
-        "port=446"
+    };
+
+    "/home/chen/nas" = {
+      device = "cracktc.tpddns.cn:/volume1/nas";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
       ];
-  };
-  fileSystems."/home/chen/nas" = {
-    device = "cracktc.tpddns.cn:/volume1/nas";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" ];
+    };
   };
 }
