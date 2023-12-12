645ffeed0307deaec3ece836af740ed35153be81
CrackTC <chenrke369@gmail.com>
2023-12-12 23:33:09 +0800

    rbw: update base_url

--- home/rofi/rbw.nix
+++ home/rofi/rbw.nix
@@ -2,14 +2,14 @@
   programs.rbw = {
     enable = true;
     settings = {
-      base_url = "http://192.168.1.100:5252";
+      base_url = "http://cracktc.tpddns.cn:5252";
       inherit (info) email;
       lock_timeout = 3600;
     };
   };
 
   home.packages = with pkgs; [
-    rofi-rbw
+    rofi-rbw-wayland
     ydotool
   ];
 }
