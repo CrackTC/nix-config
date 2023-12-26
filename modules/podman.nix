cf702a24d2937a6ea8d82fa761548b63055d218e
CrackTC <chenrke369@gmail.com>
2023-12-26 20:52:51 +0800

    use podman

---
+++ modules/podman.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }: {
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+    extraPackages = with pkgs; [ podman-compose ];
+  };
+}
