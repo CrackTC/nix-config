33911ae802b991e47a323f8ee0a50347d9afc11c
CrackTC <chenrke369@gmail.com>
2024-01-02 17:26:29 +0800

    firefox: use portal filepicker

--- home/chen/programs/firefox/settings/theme.nix
+++ home/chen/programs/firefox/settings/theme.nix
@@ -5,5 +5,6 @@ profileName: {
     "svg.context-properties.content.enabled" = true;
     "browser.tabs.tabClipWidth" = 83;
     "security.insecure_connection_text.enabled" = true;
+    "widget.use-xdg-desktop-portal.file-picker" = 1;
   };
 }
