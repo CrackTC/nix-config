16a26c67206374accdc01cdbf518732beacc1959
CrackTC <chenrke369@gmail.com>
2024-01-02 17:39:31 +0800

    sops: follow nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -20,11 +20,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master = { url = "github:NixOS/nixpkgs/master"; flake = false; };
-    nixpkgs-stable = { url = "github:nixos/nixpkgs/nixos-23.11"; flake = false; };
+    nixpkgs-stable = { url = "github:NixOS/nixpkgs/nixos-23.11"; flake = false; };
     nur = { url = "github:nix-community/NUR"; flake = false; };
-    myRepo = { url = "github:CrackTC/nur-packages"; inputs.nixpkgs.follows = "nixpkgs"; };
-    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
-    sops-nix = { url = "github:Mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    myRepo = {
+      url = "github:CrackTC/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+    };
   };
 
   outputs =
