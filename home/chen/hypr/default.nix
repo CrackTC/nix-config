21c94ccb86385fcb98e9ad4c055c1f36336c5080
CrackTC <chenrke369@gmail.com>
2024-01-05 00:13:17 +0800

    hyprland: clean

--- home/chen/hypr/default.nix
+++ home/chen/hypr/default.nix
@@ -1,27 +1,26 @@
 { useNvidia, extMonitor }: { pkgs, lib, extraRepos, info, ... }: {
   imports =
-    (if useNvidia then [ ./nvidia.nix ] else [ ./intel.nix ]) ++
-    (if !extMonitor then [ ./noExtMonitor.nix ] else [ ]) ++ [
-      ./keybindings.nix
-    ];
+    (lib.singleton (if useNvidia then ./nvidia.nix else ./intel.nix))
+    ++ (lib.optional (!extMonitor) ./noExtMonitor.nix)
+    ++ (lib.singleton ./keybindings.nix);
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
       monitor = lib.mkDefault [
-        "DP-2,2560x1440@170,2560x0,1"
-        "eDP-1,2560x1440@240,0x0,1.33"
+        "DP-1,2560x1440@170,auto,1"
+        "eDP-1,2560x1440@240,0x0,1.25"
         "monitor=HDMI-A-1,disabled"
       ];
 
       workspace = lib.mkDefault [
-        "1, monitor:DP-2"
-        "2, monitor:DP-2"
-        "3, monitor:DP-2"
-        "4, monitor:DP-2"
-        "5, monitor:DP-2"
-        "6, monitor:DP-2"
-        "7, monitor:DP-2"
-        "8, monitor:DP-2"
+        "1, monitor:DP-1"
+        "2, monitor:DP-1"
+        "3, monitor:DP-1"
+        "4, monitor:DP-1"
+        "5, monitor:DP-1"
+        "6, monitor:DP-1"
+        "7, monitor:DP-1"
+        "8, monitor:DP-1"
         "9, monitor:eDP-1"
         "10, monitor:eDP-1, default:true"
       ];
