43c491e500290beae64170188dffd7376b01d1e4
CrackTC <chenrke369@gmail.com>
2024-01-11 06:35:08 +0800

    don't put nfs directly under home dir

--- modules/mount.nix
+++ modules/mount.nix
@@ -6,31 +6,31 @@
       fsType = "ntfs";
     };
 
-    "/mnt/smb/nas" = {
-      device = "//cracktc.tpddns.cn/nas";
-      fsType = "cifs";
-      options =
-        let
-          automount_opts = [
-            "x-systemd.automount"
-            "noauto"
-            "x-systemd.idle-timeout=60"
-            "x-systemd.device-timeout=5s"
-            "x-systemd.mount-timeout=5s"
-          ];
-        in
-        automount_opts ++ [
-          "credentials=/run/secrets/nas_credentials"
-          "iocharset=utf8"
-          "uid=1000"
-          "gid=100"
-          "sec=ntlmssp"
-          "rw"
-          "port=446"
-        ];
-    };
+    # "/mnt/smb/nas" = {
+    #   device = "//cracktc.tpddns.cn/nas";
+    #   fsType = "cifs";
+    #   options =
+    #     let
+    #       automount_opts = [
+    #         "x-systemd.automount"
+    #         "noauto"
+    #         "x-systemd.idle-timeout=60"
+    #         "x-systemd.device-timeout=5s"
+    #         "x-systemd.mount-timeout=5s"
+    #       ];
+    #     in
+    #     automount_opts ++ [
+    #       "credentials=/run/secrets/nas_credentials"
+    #       "iocharset=utf8"
+    #       "uid=1000"
+    #       "gid=100"
+    #       "sec=ntlmssp"
+    #       "rw"
+    #       "port=446"
+    #     ];
+    # };
 
-    "/home/chen/nas" = {
+    "/home/chen/net/nas" = {
       device = "cracktc.tpddns.cn:/volume1/nas";
       fsType = "nfs";
       options = [
