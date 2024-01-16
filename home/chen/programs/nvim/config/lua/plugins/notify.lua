cefda553949349092de049578acc6d08daaf7f0a
CrackTC <chenrke369@gmail.com>
2024-01-16 18:23:14 +0800

    nvim: notify: clean

--- home/chen/programs/nvim/config/lua/plugins/notify.lua
+++ home/chen/programs/nvim/config/lua/plugins/notify.lua
@@ -3,81 +3,13 @@ local M = {}
 local function setup_plugin()
     require("notify").setup({
         stages = "fade",
-        render = "compact"
+        render = "default"
     })
 end
 
 local function setup_variables()
     vim.notify = require("notify")
 end
-
--- local function setup_coc_diagnostic_notification()
---     local notify = require("notify")
---     local coc_diagnostic_record = {}
---
---     local function reset_coc_diagnostic_record(_)
---         coc_diagnostic_record = {}
---     end
---
---     local function coc_diagnostic_do_notify(msg, level)
---         local notify_opts = {
---             title = "LSP Diagnostics",
---             timeout = 500,
---             on_close = reset_coc_diagnostic_record
---         }
---         if coc_diagnostic_record ~= {} then
---             notify_opts["replace"] = coc_diagnostic_record.id
---         end
---         coc_diagnostic_record = notify(msg, level, notify_opts)
---     end
---
---     local function coc_diagnostic_notify()
---         local info = vim.b.coc_diagnostic_info
---         if info == nil then
---             return ""
---         end
---
---         local messages = {}
---         local level = "info"
---
---         if info.warning > 0 then
---             level = "warn"
---         end
---
---         if info.error > 0 then
---             level = "error"
---         end
---
---         if info.error > 0 then
---             messages[#messages + 1] = "󰃤 Errors: " .. info.error
---         end
---
---         if info.warning > 0 then
---             messages[#messages + 1] = "󱐋 Warnings: " .. info.warning
---         end
---
---         if info.information > 0 then
---             messages[#messages + 1] = "󰋼 Infos: " .. info.information
---         end
---
---         if info.hint > 0 then
---             messages[#messages + 1] = "󰌵 Hints: " .. info.hint
---         end
---
---         local message = table.concat(messages, "\n")
---
---         if vim.trim(message) == "" then
---             message = " All OK"
---         end
---
---         coc_diagnostic_do_notify(message, level)
---     end
---
---     vim.api.nvim_create_autocmd("User", {
---         pattern = "CocDiagnosticChange",
---         callback = coc_diagnostic_notify
---     })
--- end
 
 local function setup_coc_status_notification()
     local notify = require("notify")
@@ -117,10 +49,6 @@ local function setup_coc_status_notification()
     })
 end
 
-local function setup_coc_notification()
-    setup_coc_status_notification()
-    -- setup_coc_diagnostic_notification()
-end
 
 local function show_welcome()
     require("notify").notify("今日もいい天気だよ～", "info", {
@@ -131,7 +59,7 @@ end
 function M.config()
     setup_plugin()
     setup_variables()
-    setup_coc_notification()
+    setup_coc_status_notification()
 
     show_welcome()
 end
