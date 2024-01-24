1650addb8a65aa45fd62c6c98e8ec7153f082460
CrackTC <chenrke369@gmail.com>
2024-01-24 21:17:53 +0800

    firefox: change scale yet again

--- home/chen/programs/firefox/settings/default.nix
+++ home/chen/programs/firefox/settings/default.nix
@@ -31,7 +31,7 @@ profileName: { pkgs, info, ... }: {
     profiles.${profileName}.settings = {
       "trailhead.firstrun.didSeeAboutWelcome" = true;
       "intl.locale.requested" = "zh-CN,en-US";
-      "layout.css.devPixelsPerPx" = -1;
+      "layout.css.devPixelsPerPx" = 1.25;
       "signon.rememberSignons" = false;
       "browser.tabs.tabmanager.enabled" = false;
     };
