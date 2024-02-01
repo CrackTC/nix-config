00db95f5c69239c3d3a3aaf358ab01f6d7b1b958
CrackTC <chenrke369@gmail.com>
2024-02-01 20:52:35 +0800

    add csharprepl

--- home/chen/dev/dotnet.nix
+++ home/chen/dev/dotnet.nix
@@ -1,9 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, extraRepos, ... }: {
   home.packages = with pkgs; [
     (with dotnetCorePackages; combinePackages [
       sdk_8_0
       sdk_7_0
       sdk_6_0
     ])
+
+    extraRepos.myRepo.csharprepl
   ];
 }
