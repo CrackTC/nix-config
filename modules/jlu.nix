275d8a9386bca98c5c3e91c5e29d2e86c7f91631
CrackTC <chenrke369@gmail.com>
2024-03-25 00:10:11 +0800

    don't run drcom on ssid mismatch

--- modules/jlu.nix
+++ modules/jlu.nix
@@ -5,7 +5,16 @@
       dispatcherScripts = [
         {
           source = pkgs.writeShellScript "jlu-drcom" ''
-            if [ "$2" = "up" ]; then
+            INTERFACE=$1
+            ACTION=$2
+
+            SSID=`${pkgs.wirelesstools}/bin/iwconfig $INTERFACE | grep ESSID | awk '{print $4}' | sed 's/ESSID:"\(.*\)"/\1/'`
+
+            if [[ `echo -e "$SSID"` != JLU* ]]; then
+                exit 0
+            fi
+
+            if [ "$ACTION" = "up" ]; then
                 pkill jlu-drcom
                 /home/${info.username}/repos/dr-jlu-linux/bin/jlu-drcom&
             fi
