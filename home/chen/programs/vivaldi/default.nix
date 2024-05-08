5482f585b40dedb81e2d9700e5f16b5b2891972d
CrackTC <chenrke369@gmail.com>
2024-05-08 22:05:04 +0800

    vivaldi: use vaapi under wayland

--- home/chen/programs/vivaldi/default.nix
+++ home/chen/programs/vivaldi/default.nix
@@ -8,7 +8,7 @@
     (vivaldi.override {
       proprietaryCodecs = true;
       enableWidevine = true;
+      commandLineArgs = "--force-dark-mode --enable-features=UseOzonePlatform --ozone-platform-hint=wayland --enable-wayland-ime --enable-features=VaapiVideoDecodeLinuxGL --use-gl=angle --use-angle=gl";
     })
-    vivaldi-ffmpeg-codecs
   ];
 }
