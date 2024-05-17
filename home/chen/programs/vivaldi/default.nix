e0dd89f9f87d3c8d0db951df7d896d7ba2cf367a
CrackTC <chenrke369@gmail.com>
2024-05-17 10:53:51 +0800

    vivaldi: remove unused codecs

--- home/chen/programs/vivaldi/default.nix
+++ home/chen/programs/vivaldi/default.nix
@@ -6,8 +6,6 @@
 
   home.packages = with pkgs; [
     (vivaldi.override {
-      proprietaryCodecs = true;
-      enableWidevine = true;
       commandLineArgs = "--force-dark-mode --enable-features=UseOzonePlatform --ozone-platform-hint=wayland --enable-wayland-ime --enable-features=VaapiVideoDecodeLinuxGL --use-gl=angle --use-angle=gl";
     })
   ];
