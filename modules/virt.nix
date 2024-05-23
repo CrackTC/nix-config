d8dab5be167011fe8fe036ddb308e1f9287ee0ed
CrackTC <chenrke369@gmail.com>
2024-05-23 12:30:53 +0800

    virt: config

--- modules/virt.nix
+++ modules/virt.nix
@@ -1,5 +1,10 @@
-{ pkgs, ... }: {
-  virtualisation.libvirtd.enable = true;
+{ pkgs, info, ... }: {
+  boot.kernelParams = [ "intel_iommu=on" "iommu=pt" ];
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.package = pkgs.qemu_kvm;
+  };
   programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = [ "root" info.username ];
   environment.systemPackages = with pkgs; [ virtiofsd spice-gtk ];
 }
