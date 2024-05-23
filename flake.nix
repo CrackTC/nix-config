bbf78772b8dc14364f13be41437aecbb9aeea20c
CrackTC <chenrke369@gmail.com>
2024-05-23 12:30:07 +0800

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   nixConfig = {
     substituters = [
-      # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
     ];
