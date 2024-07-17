e9bd40328b8bfd40f69c65476d286259d9a6413b
CrackTC <chenrke369@gmail.com>
2024-07-17 16:01:24 +0800

    spotify: wayland support

--- modules/home/programs/media/spotify.nix
+++ modules/home/programs/media/spotify.nix
@@ -1,12 +1,24 @@
 { config, pkgs, lib, ... }:
-let cfg = config.programs.media.spotify; in {
+let
+  cfg = config.programs.media.spotify;
+  spotify =
+    if config.wayland.enable then
+      (pkgs.spotify.overrideAttrs {
+        postInstall = ''
+          wrapProgram $out/share/spotify/spotify \
+            --add-flags "--enable-features=WaylandWindowDecorations --ozone-platform=wayland --gtk-version=4 --enable-wayland-ime"
+        '';
+      })
+    else pkgs.spotify;
+in
+{
   options.programs.media.spotify = {
     enable = lib.mkEnableOption "spotify";
   };
 
   config = lib.mkIf (config.gui.available && cfg.enable) {
     hmConfig = {
-      home.packages = with pkgs; [ spotify ];
+      home.packages = [ spotify ];
     };
   };
 }
