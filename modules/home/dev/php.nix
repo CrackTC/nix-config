a701a1b9716188d06704d2490055acfe23da2251
CrackTC <chenrke369@gmail.com>
2024-08-15 18:13:36 +0800

    php: add phpactor

--- modules/home/dev/php.nix
+++ modules/home/dev/php.nix
@@ -1,10 +1,10 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let cfg = config.php; in {
   options.php = {
     enable = lib.mkEnableOption "php";
   };
 
   config.hmConfig = lib.mkIf cfg.enable {
-    home.packages = [ ];
+    home.packages = [ pkgs.phpactor ];
   };
 }
