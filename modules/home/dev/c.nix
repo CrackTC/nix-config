6bc733b9adbc745e0e0453039be223cd28c8d27f
CrackTC <chenrke369@gmail.com>
2024-09-08 23:13:19 +0800

    dev: mingw cross

--- modules/home/dev/c.nix
+++ modules/home/dev/c.nix
@@ -12,6 +12,19 @@ let cfg = config.c; in {
       gnumake
       bear
       valgrind # for memory profiling
+
+      # https://discourse.nixos.org/t/statically-linked-mingw-binaries/38395/4
+      (
+        let
+          inherit (pkgs.pkgsCross.mingwW64) buildPackages;
+        in
+        buildPackages.wrapCC (buildPackages.gcc-unwrapped.override {
+          threadsCross = {
+            model = "win32";
+            package = null;
+          };
+        })
+      )
     ];
   };
 }
