04fba074cf0b87767bc215fd68a3fd822643a0a2
CrackTC <chenrke369@gmail.com>
2024-10-30 01:09:51 +0800

    discord: wayland support

--- modules/home/programs/im/discord.nix
+++ modules/home/programs/im/discord.nix
@@ -6,7 +6,14 @@ let cfg = config.programs.im.discord; in {
 
   config = lib.mkIf (config.gui.available && cfg.enable) {
     hmConfig = {
-      home.packages = [ pkgs.discord ];
+      home.packages = [
+        (pkgs.discord.overrideAttrs (oldAttrs: {
+          postInstall = oldAttrs.postInstall + ''
+            wrapProgram $out/opt/Discord/Discord \
+              --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WaylandWindowDecorations --use-gl=desktop"
+          '';
+        }))
+      ];
     };
   };
 }
