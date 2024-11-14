ba8ff6cfdbc44db23baded9c2a2b45b676bdfab2
CrackTC <chenrke369@gmail.com>
2024-11-15 01:03:33 +0800

    add LiteLoaderQQNT

--- modules/home/programs/im/qq.nix
+++ modules/home/programs/im/qq.nix
@@ -1,5 +1,20 @@
 { config, pkgs, lib, ... }:
-let cfg = config.programs.im.qq; in {
+let
+  cfg = config.programs.im.qq;
+  qq-orig = pkgs.qq.override {
+    commandLineArgs =
+      if config.wayland.enable
+      then "--ozone-platform-hint=auto --enable-wayland-ime --gtk-version=4"
+      else "";
+  };
+  liteloaderqqnt = pkgs.fetchFromGitHub {
+    owner = "LiteLoaderQQNT";
+    repo = "LiteLoaderQQNT";
+    rev = "1.2.3";
+    hash = "sha256-R/CXxweWScfe3ktygnoXKh0XFkURG+lsFGFY7KRaHJg=";
+  };
+in
+{
   options.programs.im.qq = {
     enable = lib.mkEnableOption "qq";
   };
@@ -7,12 +22,13 @@ let cfg = config.programs.im.qq; in {
   config = lib.mkIf (config.gui.available && cfg.enable) {
     hmConfig = {
       home.packages = [
-        (pkgs.qq.override {
-          commandLineArgs =
-            if config.wayland.enable
-            then "--ozone-platform-hint=auto --enable-wayland-ime --gtk-version=4"
-            else "";
-        })
+        (qq-orig.overrideAttrs (oldAttrs: {
+          postInstall = ''
+            echo 'require(String.raw`${liteloaderqqnt}`)' > $out/opt/QQ/resources/app/app_launcher/patch.js
+            sed -i $out/opt/QQ/resources/app/package.json -e 's/"main":.*/"main": "\.\/app_launcher\/patch.js",/'
+            wrapProgramShell $out/bin/qq --run 'export LITELOADERQQNT_PROFILE=$HOME/.local/share/liteloaderqqnt'
+          '';
+        }))
       ];
     };
   };
