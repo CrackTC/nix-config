d62e2c3a0fd7d293ca4b17bc2346287d30ab8ca5
CrackTC <chenrke369@gmail.com>
2024-11-19 00:02:50 +0800

    fix substituters

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   description = "Sorac no NixOS Flake";
 
   nixConfig = {
-    substituters = [
+    extra-substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://cracktc.cachix.org"
       "https://hyprland.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cracktc.cachix.org-1:2hSlXvkhNchqB0wo+nz13bWdJo9/nxrAi/masgZCm2I="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
