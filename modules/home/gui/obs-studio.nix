480d600d1352e3d3c90b9dff242afbf3fe893382
CrackTC <chenrke369@gmail.com>
2024-12-09 11:52:25 +0800

    obs-studio: plugins

--- modules/home/gui/obs-studio.nix
+++ modules/home/gui/obs-studio.nix
@@ -1,8 +1,11 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 let cfg = config.obs-studio; in {
   options.obs-studio = {
     enable = lib.mkEnableOption "obs-studio";
   };
 
-  config.hmConfig.programs.obs-studio.enable = lib.mkIf (config.gui.available && cfg.enable) true;
+  config.hmConfig.programs.obs-studio = lib.mkIf (config.gui.available && cfg.enable) {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [ ];
+  };
 }
