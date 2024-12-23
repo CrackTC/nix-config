1ac7d9f99814bfcd20db9dfedf9bf65286a82801
CrackTC <chenrke369@gmail.com>
2024-12-23 22:13:38 +0800

    chore: format

--- config/hosts/cno/default.nix
+++ config/hosts/cno/default.nix
@@ -83,29 +83,29 @@
       device = "/dev/disk/by-uuid/f542c9db-e4ae-406b-a7e0-bd0bc9cc7a61";
     };
 
-    "/mnt/smb/nas" = {
-      device = "//nas.sora.zip/nas";
-      fsType = "cifs";
-      options =
-        let
-          automount_opts = [
-            "x-systemd.automount"
-            "noauto"
-            "x-systemd.idle-timeout=60"
-            "x-systemd.device-timeout=5s"
-            "x-systemd.mount-timeout=5s"
-          ];
-        in
-        automount_opts ++ [
-          "credentials=/run/secrets/nas_credentials"
-          "iocharset=utf8"
-          "uid=1000"
-          "gid=100"
-          "sec=ntlmssp"
-          "rw"
-          "port=446"
-        ];
-    };
+    # "/mnt/smb/nas" = {
+    #   device = "//nas.sora.zip/nas";
+    #   fsType = "cifs";
+    #   options =
+    #     let
+    #       automount_opts = [
+    #         "x-systemd.automount"
+    #         "noauto"
+    #         "x-systemd.idle-timeout=60"
+    #         "x-systemd.device-timeout=5s"
+    #         "x-systemd.mount-timeout=5s"
+    #       ];
+    #     in
+    #     automount_opts ++ [
+    #       "credentials=/run/secrets/nas_credentials"
+    #       "iocharset=utf8"
+    #       "uid=1000"
+    #       "gid=100"
+    #       "sec=ntlmssp"
+    #       "rw"
+    #       "port=446"
+    #     ];
+    # };
 
     # "/home/chen/net/nas" = {
     #   device = "cracktc.tpddns.cn:/volume1/nas";
