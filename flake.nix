7b83db367c803595d1ef4a9ee3633582fa55ce60
CrackTC <chenrke369@gmail.com>
2025-01-07 08:41:17 +0800

    flake.lock: update 25-01-07

--- flake.nix
+++ flake.nix
@@ -20,78 +20,143 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-master = { url = "github:NixOS/nixpkgs/master"; flake = false; };
-    nixpkgs-stable = { url = "github:NixOS/nixpkgs/nixos-24.11"; flake = false; };
-    nixpkgs-mine = { url = "github:CrackTC/nixpkgs"; flake = false; };
-    hyprland = { url = "github:hyprwm/hyprland/b9f110ef8726fcba2b4ee69856027731e73003a5"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixpkgs-master = {
+      url = "github:NixOS/nixpkgs/master";
+      flake = false;
+    };
+    nixpkgs-stable = {
+      url = "github:NixOS/nixpkgs/nixos-24.11";
+      flake = false;
+    };
+    nixpkgs-mine = {
+      url = "github:CrackTC/nixpkgs";
+      flake = false;
+    };
+
+    myRepo = {
+      url = "github:CrackTC/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+      flake = false;
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/hyprland/b9f110ef8726fcba2b4ee69856027731e73003a5";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     ghostty.url = "github:ghostty-org/ghostty/main";
-    nur = { url = "github:nix-community/NUR"; flake = false; };
-    myRepo = { url = "github:CrackTC/nur-packages"; inputs.nixpkgs.follows = "nixpkgs"; };
-    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
-    sops-nix.url = "github:Mic92/sops-nix";
+
+    nil = {
+      url = "github:oxalica/nil";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs
-    , nixpkgs-master
-    , nixpkgs-stable
-    , nixpkgs-mine
-    , hyprland
-    , ghostty
-    , nur
-    , myRepo
-    , home-manager
-    , sops-nix
-    , ...
+    {
+      nixpkgs,
+      nixpkgs-master,
+      nixpkgs-stable,
+      nixpkgs-mine,
+      hyprland,
+      ghostty,
+      nil,
+      nur,
+      myRepo,
+      home-manager,
+      sops-nix,
+      ...
     }:
     let
-      hosts = [ "cno" ];
-      utilities = import ./utils;
+      inherit (nixpkgs) lib;
+      utilities = import ./utils { inherit lib; };
+
+      getSubDirs =
+        path:
+        builtins.readDir path |> lib.attrsets.filterAttrs (_: v: v == "directory") |> builtins.attrNames;
+
+      hosts = getSubDirs ./config/hosts;
+      homes =
+        getSubDirs ./config/homes
+        |> map (name: {
+          inherit name;
+          value = import (./config/homes/. + "/${name}");
+        })
+        |> builtins.listToAttrs;
     in
     {
-      nixosConfigurations = builtins.listToAttrs (map
-        (host-name: {
-          name = host-name;
-          value = nixpkgs.lib.nixosSystem
-            (
-              let
-                host-info = import ./config/hosts/${host-name}/flake-info.nix;
-                option = utilities.nixpkgsOptions host-info.system host-info.cpu false;
+      nixosConfigurations =
+        hosts
+        |> map (
+          host-name:
+          let
+            host-info = import ./config/hosts/${host-name}/flake-info.nix;
 
-                pkgs = import nixpkgs option;
-                extraRepos = {
-                  # pkgs-native = import nixpkgs (lib.utils.nixpkgsOptions host-info.system host-info.cpu true);
-                  pkgs-master = import nixpkgs-master option;
-                  pkgs-stable = import nixpkgs-stable option;
-                  pkgs-mine = import nixpkgs-mine option;
+            options = utilities.nixpkgsOptions { inherit (host-info) system cpu; };
 
-                  hyprland = hyprland.packages.${host-info.system};
-                  ghostty = ghostty.packages.${host-info.system};
-                  nur = import nur { inherit pkgs; nurpkgs = pkgs; };
-                  myRepo = myRepo.legacyPackages.${host-info.system};
-                };
-                specialArgs = { inherit pkgs extraRepos; inherit utilities; };
-              in
-              {
-                inherit specialArgs;
-                modules = [
-                  sops-nix.nixosModules.sops
-                  home-manager.nixosModules.home-manager
-
-                  {
-                    sorac = {
-                      host = host-info // { name = host-name; };
-                      homes = import ./config/homes;
-                    };
-                  }
-
-                  ./config/hosts/${host-name}
-                  ./modules/host
-                  ./modules/home
-                ];
+            pkgs = import nixpkgs (
+              options
+              // {
+                overlays = [ nur.overlays.default ];
               }
             );
-        })
-        hosts);
+
+            extraRepos = {
+              # pkgs-native = import nixpkgs (lib.utils.nixpkgsOptions host-info.system host-info.cpu true);
+              pkgs-master = import nixpkgs-master options;
+              pkgs-stable = import nixpkgs-stable options;
+              pkgs-mine = import nixpkgs-mine options;
+              myRepo = import myRepo { inherit pkgs; };
+
+              hyprland = hyprland.packages.${host-info.system};
+              ghostty = ghostty.packages.${host-info.system};
+              nil = nil.packages.${host-info.system};
+            };
+
+            specialArgs = {
+              inherit extraRepos;
+              inherit utilities;
+            };
+          in
+          {
+            name = host-name;
+            value = nixpkgs.lib.nixosSystem {
+              inherit specialArgs;
+              modules = [
+                sops-nix.nixosModules.sops
+                home-manager.nixosModules.home-manager
+
+                ./modules/host
+                ./modules/home
+
+                {
+                  nixpkgs.pkgs = pkgs;
+                  sorac = {
+                    host = host-info // {
+                      name = host-name;
+                    };
+                    inherit homes;
+                  };
+                }
+
+                ./config/hosts/${host-name}
+              ];
+            };
+          }
+        )
+        |> builtins.listToAttrs;
     };
 }
