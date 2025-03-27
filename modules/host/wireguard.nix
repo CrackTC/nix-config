2eeeb4774e3d234cbd8ef98901a8a89e518c2038
CrackTC <chenrke369@gmail.com>
2025-03-27 17:28:22 +0800

    favor wg-quick

--- modules/host/wireguard.nix
+++ modules/host/wireguard.nix
@@ -6,25 +6,20 @@ in
     sops.secrets.wireguard_private_key = { };
     networking = {
       firewall.allowedUDPPorts = [ listenPort ];
-      wireguard = {
-        enable = true;
-        interfaces = {
-          wg0 = {
-            ips = [ "192.168.96.100/24" ];
-            inherit listenPort;
-            privateKeyFile = "/run/secrets/wireguard_private_key";
+      wg-quick.interfaces = {
+        wg0 = {
+          address = [ "192.168.96.100/24" ];
+          privateKeyFile = "/run/secrets/wireguard_private_key";
 
-            peers = [
-              {
-                publicKey = "e/KvjAa+7mQt+RgC1+cSQMiWipaVu8d/hIbrgVT/uDY=";
-                allowedIPs = [ "192.168.96.0/24" ];
-                endpoint = "nas.sora.zip:39353";
-              }
-            ];
-          };
+          peers = [
+            {
+              publicKey = "e/KvjAa+7mQt+RgC1+cSQMiWipaVu8d/hIbrgVT/uDY=";
+              allowedIPs = [ "192.168.96.0/24" ];
+              endpoint = "nas.sora.zip:39353";
+            }
+          ];
         };
       };
     };
   };
 }
-
