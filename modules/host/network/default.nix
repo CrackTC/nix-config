874f5a6b867ac1760957869154eb3b94985e9aa1
CrackTC <chenrke369@gmail.com>
2025-04-23 16:01:57 +0800

    networking: favor iwd

--- modules/host/network/default.nix
+++ modules/host/network/default.nix
@@ -14,26 +14,32 @@ in
     description = "Host is in JLU :(";
   };
 
-  config.networking.networkmanager = {
-    enable = true;
-    dispatcherScripts = lib.mkIf cfg.inJLU [
-      {
-        source = pkgs.writeShellScript "jlu-drcom" ''
-          INTERFACE=$1
-          ACTION=$2
+  config.networking = {
+    networkmanager = {
+      enable = true;
+      wifi = {
+        backend = "iwd";
+        powersave = true;
+      };
+      dispatcherScripts = lib.mkIf cfg.inJLU [
+        {
+          source = pkgs.writeShellScript "jlu-drcom" ''
+            INTERFACE=$1
+            ACTION=$2
 
-          SSID=`${pkgs.wirelesstools}/bin/iwconfig $INTERFACE | grep ESSID | ${lib.getExe pkgs.gawk} '{print $4}' | sed 's/ESSID:"\(.*\)"/\1/'`
+            SSID=`${pkgs.wirelesstools}/bin/iwconfig $INTERFACE | grep ESSID | ${lib.getExe pkgs.gawk} '{print $4}' | sed 's/ESSID:"\(.*\)"/\1/'`
 
-          if [[ `echo -e "$SSID"` != JLU* ]]; then
-              exit 0
-          fi
+            if [[ `echo -e "$SSID"` != JLU* ]]; then
+                exit 0
+            fi
 
-          if [ "$ACTION" = "up" ]; then
-              ${pkgs.procps}/bin/pkill jlu-drcom
-              /home/chen/repos/dr-jlu-linux/bin/jlu-drcom&
-          fi
-        '';
-      }
-    ];
+            if [ "$ACTION" = "up" ]; then
+                ${pkgs.procps}/bin/pkill jlu-drcom
+                /home/chen/repos/dr-jlu-linux/bin/jlu-drcom&
+            fi
+          '';
+        }
+      ];
+    };
   };
 }
