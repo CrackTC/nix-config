485da407ce59be6679061bd0e45bf3b820146478
CrackTC <chenrke369@gmail.com>
2025-04-28 00:50:35 +0800

    metasploit: update

--- modules/home/dev/ctf/metasploit.nix
+++ modules/home/dev/ctf/metasploit.nix
@@ -6,15 +6,6 @@
 }:
 let
   cfg = config.metasploit;
-  metasploit = pkgs.metasploit.overrideAttrs {
-    version = "unstable-24-07-20";
-    src = pkgs.fetchFromGitHub {
-      owner = "rapid7";
-      repo = "metasploit-framework";
-      rev = "db5ace8c5f839d7471fd503a1758e65105502cd8";
-      hash = "sha256-BA83dSAUkqINdGV2JqLhdC0d7QIUz/iNCrjv080zYMQ=";
-    };
-  };
 in
 {
   options.metasploit = {
@@ -22,6 +13,6 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    hmConfig.home.packages = [ metasploit ];
+    hmConfig.home.packages = [ pkgs.metasploit ];
   };
 }
