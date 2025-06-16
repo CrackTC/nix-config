dbf13433549e6f1c94d3b810026bbb05ae8fad3d
CrackTC <chenrke369@gmail.com>
2025-06-16 22:58:32 +0800

    chrome: flags

--- modules/home/gui/chrome/default.nix
+++ modules/home/gui/chrome/default.nix
@@ -14,7 +14,18 @@ in
 
   config.hmConfig = lib.mkIf (config.gui.available && cfg.enable) {
     home.packages = [
-      pkgs.google-chrome
+      (pkgs.google-chrome.override {
+        commandLineArgs =
+          if config.wayland.enable then
+            (builtins.concatStringsSep " " [
+              "--enable-features=UseOzonePlatform"
+              "--ozone-platform-hint=wayland"
+              "--enable-wayland-ime"
+              "--enable-features=AcceleratedVideoDecodeLinuxGL"
+            ])
+          else
+            "";
+      })
     ];
   };
 }
