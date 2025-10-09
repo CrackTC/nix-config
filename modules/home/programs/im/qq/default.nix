4462ba52dac093865af62a238b71104cc03ed522
CrackTC <chenrke369@gmail.com>
2025-10-09 19:04:01 +0800

    qq: update

--- modules/home/programs/im/qq/default.nix
+++ modules/home/programs/im/qq/default.nix
@@ -6,34 +6,33 @@
 }:
 let
   cfg = config.programs.im.qq;
-
-  sources = import ./sources.nix;
-  srcs = {
-    x86_64-linux = pkgs.fetchurl {
-      url = sources.amd64_url;
-      hash = sources.amd64_hash;
-    };
-    aarch64-linux = pkgs.fetchurl {
-      url = sources.arm64_url;
-      hash = sources.arm64_hash;
-    };
-  };
-  src =
-    srcs.${pkgs.stdenv.hostPlatform.system}
-      or (throw "Unsupported system: ${pkgs.stdenv.hostPlatform.system}");
-
-  qq-orig =
-    (pkgs.qq.overrideAttrs {
-      inherit src;
-      inherit (sources) version;
-    }).override
-      {
-        commandLineArgs =
-          if config.wayland.enable then
-            "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime"
-          else
-            "";
-      };
+  # sources = import ./sources.nix;
+  # srcs = {
+  #   x86_64-linux = pkgs.fetchurl {
+  #     url = sources.amd64_url;
+  #     hash = sources.amd64_hash;
+  #   };
+  #   aarch64-linux = pkgs.fetchurl {
+  #     url = sources.arm64_url;
+  #     hash = sources.arm64_hash;
+  #   };
+  # };
+  # src =
+  #   srcs.${pkgs.stdenv.hostPlatform.system}
+  #     or (throw "Unsupported system: ${pkgs.stdenv.hostPlatform.system}");
+  #
+  # qq-orig =
+  #   (pkgs.qq.overrideAttrs {
+  #     inherit src;
+  #     inherit (sources) version;
+  #   }).override
+  #     {
+  #       commandLineArgs =
+  #         if config.wayland.enable then
+  #           "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime"
+  #         else
+  #           "";
+  #     };
   # liteloaderqqnt = pkgs.fetchFromGitHub {
   #   owner = "LiteLoaderQQNT";
   #   repo = "LiteLoaderQQNT";
@@ -49,7 +48,14 @@ in
   config = lib.mkIf (config.gui.available && cfg.enable) {
     hmConfig = {
       home.packages = [
-        qq-orig
+        (pkgs.qq.override {
+          commandLineArgs =
+            if config.wayland.enable then
+              "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true --wayland-text-input-version=3"
+            else
+              "";
+        })
+
         # (qq-orig.overrideAttrs (oldAttrs: {
         #   postInstall = ''
         #     echo 'require(String.raw`${liteloaderqqnt}`)' > $out/opt/QQ/resources/app/app_launcher/patch.js
