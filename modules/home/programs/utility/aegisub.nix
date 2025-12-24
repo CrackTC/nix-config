f22aa73b21b4ede9b01bf81c69fe193d2f7f0427
CrackTC <chenrke369@gmail.com>
2025-12-24 23:23:56 +0800

    aegisub: use lastest git & fix build

--- modules/home/programs/utility/aegisub.nix
+++ modules/home/programs/utility/aegisub.nix
@@ -14,7 +14,19 @@ in
 
   config.hmConfig = lib.mkIf cfg.enable {
     home.packages = [
-      pkgs.aegisub
+      (pkgs.aegisub.overrideAttrs (oldAttrs: {
+        src = pkgs.fetchFromGitHub {
+          owner = "TypesettingTools";
+          repo = "aegisub";
+          rev = "8a7d53801d2a41336f95d1db4802c97330ab9ea0";
+          hash = "sha256-6hjVNM2OZKZ7hAty+jbaqEXq/OAJw+WVJeUBOVQY/II=";
+        };
+
+        postPatch = ''
+          patchShebangs tools/combine-config.py
+        ''
+        + oldAttrs.postPatch;
+      }))
     ];
   };
 }
