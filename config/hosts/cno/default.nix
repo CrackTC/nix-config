3d0403e9eb6329978b146aa0e17e8fc313bfcaf1
CrackTC <chenrke369@gmail.com>
2025-12-27 16:55:59 +0800

    host: use identical scale to reduce display bugs

--- config/hosts/cno/default.nix
+++ config/hosts/cno/default.nix
@@ -47,14 +47,14 @@
         # left = builtins.ceil (3840 / 2);
         left = 0;
         top = 0;
-        scale = 1.25;
+        scale = 2.0;
       }
       {
         name = "DP-1";
         weight = 3840;
         height = 2160;
         refresh = 60;
-        left = builtins.ceil (2560 / 1.25);
+        left = builtins.ceil (2560 / 2);
         top = 0;
         scale = 2.0;
       }
