9010729506bc760f7d703b19bddd3d700746ee7a
CrackTC <chenrke369@gmail.com>
2026-01-04 01:04:35 +0800

    aegisub: add karaoke-helper

--- modules/home/programs/utility/aegisub.nix
+++ modules/home/programs/utility/aegisub.nix
@@ -6,6 +6,12 @@
 }:
 let
   cfg = config.programs.utility.aegisub;
+  karaoke-helper = pkgs.fetchFromGitHub {
+    owner = "CrackTC";
+    repo = "aegisub-karaoke-helper";
+    rev = "c00f5ed42b943e863e59b0b6e40207429b91302e";
+    hash = "sha256-GTVM5K+z+O2lnpPj12tgwUVJKzjWjdZmc+DXpnCToh0=";
+  };
 in
 {
   options.programs.utility.aegisub = {
@@ -18,14 +24,18 @@ in
         src = pkgs.fetchFromGitHub {
           owner = "TypesettingTools";
           repo = "aegisub";
-          rev = "8a7d53801d2a41336f95d1db4802c97330ab9ea0";
-          hash = "sha256-6hjVNM2OZKZ7hAty+jbaqEXq/OAJw+WVJeUBOVQY/II=";
+          rev = "eef3fcc6cbf9bfd209b597258c63f8b26b8c1246";
+          hash = "sha256-oEuBzgb1AKjNu1KgCb5Esb7jyBwL/31bTvOaz5k+97M=";
         };
 
         postPatch = ''
           patchShebangs tools/combine-config.py
         ''
         + oldAttrs.postPatch;
+
+        postInstall = oldAttrs.postInstall + ''
+          cp ${karaoke-helper}/karaoke-helper.lua $out/share/aegisub/automation/autoload/
+        '';
       }))
     ];
   };
